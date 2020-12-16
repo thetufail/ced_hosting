d294a7ea4d29e5a622fdd891611800df41721e27
cedcoss <you@example.com>
2020-12-16 12:27:13 +0530

    dummy commit

--- account.php
+++ account.php
@@ -16,8 +16,6 @@ if (isset($_POST['signup'])) {
     if ($password != $repassword || $name == ' ' || $name == ' ' || $mobile == ' ') {
         $errors[] = array('input' => 'password', 'message' => 'password doesn\'t match');
     }
-
-    $_SESSION['email'] = $_POST['email'];
     $sign_up_user = new User();
     $sign_up_user->signup($email, $name, $mobile, 0, 0, 0, 0, $sign_up_date, $password, $security_question, $security_answer, $db->conn);
 }
@@ -31,108 +29,98 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 -->
 <!DOCTYPE HTML>
 <html>
-
 <head>
-    <title>Planet Hosting a Hosting Category Flat Bootstrap Responsive Website Template | Account :: w3layouts</title>
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="keywords" content="Planet Hosting Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+<title>Planet Hosting a Hosting Category Flat Bootstrap Responsive Website Template | Account :: w3layouts</title>
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Planet Hosting Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-    <script type="application/x-javascript">
-        addEventListener("load", function() {
-            setTimeout(hideURLbar, 0);
-        }, false);
-
-        function hideURLbar() {
-            window.scrollTo(0, 1);
-        }
-    </script>
-    <script src="js/jquery-1.11.1.min.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <!---fonts-->
-    <link href='//fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
-    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
-    <!---fonts-->
-    <!--script-->
-    <link rel="stylesheet" href="css/swipebox.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="js/jquery.swipebox.min.js"></script>
-    <script type="text/javascript">
-        jQuery(function($) {
-            $(".swipebox").swipebox();
-        });
-    </script>
-    <!--script-->
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<!---fonts-->
+<link href='//fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+<!---fonts-->
+<!--script-->
+<link rel="stylesheet" href="css/swipebox.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+			<script src="js/jquery.swipebox.min.js"></script> 
+			    <script type="text/javascript">
+					jQuery(function($) {
+						$(".swipebox").swipebox();
+					});
+				</script>
+<!--script-->
 </head>
-
 <body>
-    <?php include_once 'header.php'; ?>
+<?php include_once 'header.php'; ?>
 
-    <!---login--->
-    <div class="content">
-        <!-- registration -->
-        <div class="main-1">
-            <div class="container">
-                <div class="register">
-                    <form method="POST">
-                        <div class="register-top-grid">
-                            <h3>personal information</h3>
-                            <div>
-                                <span>Name<label>*</label></span>
-                                <input type="text" name="name" class="name" pattern="^[a-zA-Z]+( [a-zA-Z]+)*$" required>
-                            </div>
-                            <div>
-                                <span>Email Address<label>*</label></span>
-                                <input type="email" name="email" class="email" pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" title="yourname@example.com" required>
-                            </div>
-                            <div>
-                                <!-- ^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$ -->
-                                <span>Mobile<label>*</label></span>
-                                <input type="text" name="mobno" class="mobno" pattern="(^0?([1-9]{1})([0-9]{9}))" required>
-                            </div>
-                            <div>
-                                <span>Security Question<label>*</label></span>
-                                <select id="security_question" name="security_question">
-                                    <option selected>Choose one of the following as security question</option>
-                                    <option value="What was your childhood nickname?">What was your childhood nickname?</option>
-                                    <option value="What is the name of your favourite childhood friend?">What is the name of your favourite childhood friend?</option>
-                                    <option value="What was your favourite place to visit as a child?">What was your favourite place to visit as a child?</option>
-                                    <option value="What was your dream job as a child?">What was your dream job as a child?</option>
-                                    <option value="What is your favourite teacher's nickname?">What is your favourite teacher's nickname?</option>
-                                </select>
-                                <input type="text" name="security_answer" title="AlphaNumeric are allowed" pattern="^[0-9]*[A-Za-z]+[A-Za-z0-9]*$">
-                            </div>
-                            <!-- pattern="^(([0-9][a-zA-Z]) | ([a-zA-Z][0-9]?) | ([a-zA-Z]))" -->
-                            <!-- pattern="^([a-z+A-Z+0-9+]|([a-zA-Z]+))$" -->
-                            <div class="clearfix"> </div>
-                            <a class="news-letter" href="#">
-                                <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>Sign Up for Newsletter</label>
-                            </a>
+<!---login--->
+<div class="content">
+    <!-- registration -->
+    <div class="main-1">
+        <div class="container">
+            <div class="register">
+                <form method="POST">
+                    <div class="register-top-grid">
+                        <h3>personal information</h3>
+                        <div>
+                            <span>Name<label>*</label></span>
+                            <input type="text" name="name" class="name" pattern="^[a-zA-Z]+( [a-zA-Z]+)*$" required>
                         </div>
-                        <div class="register-bottom-grid">
-                            <h3>login information</h3>
-                            <div>
-                                <span>Password<label>*</label></span>
-                                <input type="password" name="pass" pattern="^[a-zA-Z0-9/_*/@*/.*/#*/%*/!*/=*/$*]{8,16}$" title="should contain a-z, A-Z, 0-9, _, @, ., #, %, !, $" required>
-                            </div>
-                            <div>
-                                <span>Confirm Password<label>*</label></span>
-                                <input type="password" name="repass" pattern="^[a-zA-Z0-9/_*/@*/.*/#*/%*/!*/=*/$*]{8,16}$" title="should contain a-z, A-Z, 0-9, _, @, ., #, %, !, $" required>
-                            </div>
+                        <div>
+                            <span>Email Address<label>*</label></span>
+                            <input type="email" name="email" class="email" pattern="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" title="yourname@example.com" required>
                         </div>
+                        <div>
+                            <!-- ^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$ -->
+                            <span>Mobile<label>*</label></span>
+                            <input type="text" name="mobno" class="mobno" pattern="(^0?([1-9]{1})([0-9]{9}))" required>
+                        </div>
+                        <div>
+                            <span>Security Question<label>*</label></span>
+                            <select id="security_question" name="security_question">
+                                <option selected>Choose one of the following as security question</option>
+                                <option value="What was your childhood nickname?">What was your childhood nickname?</option>
+                                <option value="What is the name of your favourite childhood friend?">What is the name of your favourite childhood friend?</option>
+                                <option value="What was your favourite place to visit as a child?">What was your favourite place to visit as a child?</option>
+                                <option value="What was your dream job as a child?">What was your dream job as a child?</option>
+                                <option value="What is your favourite teacher's nickname?">What is your favourite teacher's nickname?</option>
+                            </select>
+                            <input type="text" name="security_answer" title="AlphaNumeric are allowed" pattern="^[0-9]*[A-Za-z]+[A-Za-z0-9]*$">
+                        </div>
+                        <!-- pattern="^(([0-9][a-zA-Z]) | ([a-zA-Z][0-9]?) | ([a-zA-Z]))" -->
+                        <!-- pattern="^([a-z+A-Z+0-9+]|([a-zA-Z]+))$" -->
                         <div class="clearfix"> </div>
-                        <div class="register-but">
-                            <input type="submit" name="signup" value="submit">
-                            <div class="clearfix"> </div>
+                        <a class="news-letter" href="#">
+                            <label class="checkbox"><input type="checkbox" name="checkbox" checked=""><i> </i>Sign Up for Newsletter</label>
+                        </a>
+                    </div>
+                    <div class="register-bottom-grid">
+                        <h3>login information</h3>
+                        <div>
+                            <span>Password<label>*</label></span>
+                            <input type="password" name="pass" pattern="^[a-zA-Z0-9/_*/@*/.*/#*/%*/!*/=*/$*]{8,16}$" title="should contain a-z, A-Z, 0-9, _, @, ., #, %, !, $" required>
                         </div>
-                    </form>
-                </div>
+                        <div>
+                            <span>Confirm Password<label>*</label></span>
+                            <input type="password" name="repass" pattern="^[a-zA-Z0-9/_*/@*/.*/#*/%*/!*/=*/$*]{8,16}$" title="should contain a-z, A-Z, 0-9, _, @, ., #, %, !, $" required>
+                        </div>
+                    </div>
+                    <div class="clearfix"> </div>
+                    <div class="register-but">
+                        <input type="submit" name="signup" value="submit">
+                        <div class="clearfix"> </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    <!-- registration -->
+</div>
+<!-- registration -->
 
-    <!-- login -->
-    <?php include_once 'footer.php'; ?>
+<!-- login -->
+<?php include_once 'footer.php'; ?>
--- verification.php
+++ verification.php
@@ -1,95 +1,19 @@
 <?php session_start(); ?>
-<?php require_once 'admin/Dbcon.php'; ?>
-<?php require_once 'admin/User.php'; ?>
 <?php
 
-if (isset($_POST['verifyOTPbyEmail'])) {
-    $opt_by_email = $_POST['enterOTPbyemail'];
-    if (isset($_SESSION['otp']) && $opt_by_email == $_SESSION['otp']) {
-        $update_signup_information = new User();
-        $update_signup_information->update_signup_information($_SESSION['id'], $db->conn);
-    } else { ?>
-        <script>
-            alert('OTP verification failed. Please re-enter OTP.');
-        </script>
-<?php
-
-    }
-}
+require_once 'admin/Dbcon.php';
+include_once 'header.php';
 
 ?>
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE HTML>
-<html>
 
-<head>
-    <title>Planet Hosting a Hosting Category Flat Bootstrap Responsive Website Template | Home :: w3layouts</title>
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="keywords" content="Planet Hosting Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
-    <script type="application/x-javascript">
-        addEventListener("load", function() {
-            setTimeout(hideURLbar, 0);
-        }, false);
-
-        function hideURLbar() {
-            window.scrollTo(0, 1);
-        }
-    </script>
-    <script src="js/jquery-1.11.1.min.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <!---fonts-->
-    <link href='//fonts.googleapis.com/css?family=Voltaire' rel='stylesheet' type='text/css'>
-    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
-    <!---fonts-->
-    <!--script-->
-    <script src="js/modernizr.custom.97074.js"></script>
-    <script src="js/jquery.chocolat.js"></script>
-    <link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--lightboxfiles-->
-    <script type="text/javascript">
-        $(function() {
-            $('.team a').Chocolat();
-        });
-    </script>
-    <script type="text/javascript" src="js/jquery.hoverdir.js"></script>
-    <script type="text/javascript">
-        $(function() {
-
-            $(' #da-thumbs > li ').each(function() {
-                $(this).hoverdir();
-            });
-
-        });
-    </script>
-    <!--script-->
-</head>
-
-<body>
-    <?php include_once 'header.php';
-
-    ?>
-
-    <form method="POST">
-        <div class="verify">
-            <p class="h4 py-2">Please enter the OTP</p>
-            <!-- <input name="verifyEmail" type="hidden"> -->
-            <!-- <input name="verifyMobile" type="hidden"> -->
-            <input name="enterOTPbyemail" type="text">
-            <input type="submit" name="verifyOTPbyEmail" value="Verify Email">
-            <p class="h5 text-center">OR</p>
-            <input name="enterOTPbymobile" type="text">
-            <input type="submit" name="verifyOTPbyMobile" value="Verify Mobile">
-        </div>
-        <!-- <div class="clearfix"></div> -->
-    </form>
-    <?php include_once 'footer.php'; ?>
+            <form method="POST">
+                <div class="verify">
+                    <h1></h1>
+                    <input name="verifyEmail" type="hidden">
+                    <input name="verifyMobile" type="hidden">
+                    <input type="submit" name="verifyEmail" value="Verify Email">
+                    <input type="submit" name="verifyMobile" value="Verify Mobile">
+                </div>
+                <!-- <div class="clearfix"></div> -->
+            </form>
+<?php include_once 'footer.php'; ?>
